4c97484e61436e21ea8db63802707ac5c9e4ba53
Colin Blundell <blundell@chromium.org>
2017-10-16 15:02:33 +0200

    Add test framework

---
+++ test/run_tests.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import os
+import sys
+import unittest
+
+# Add the parent directory to the path.
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from dummy_test import *
+
+if __name__ == '__main__':
+  unittest.main()
