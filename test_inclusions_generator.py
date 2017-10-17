dcb32e260963030a074def76c88b9d78b7b1e78d
Colin Blundell <blundell@chromium.org>
2017-10-17 14:42:18 +0200

    Add tests for new functionality

--- test_inclusions_generator.py
+++ test_inclusions_generator.py
@@ -34,6 +34,27 @@ class TestInclusionsGenerator(unittest.TestCase):
     self.assertEqual(inclusions_of_foo, ["bar/bar.h", "bar/core.h",
                                          "foo/foo.cc"])
 
+  def verify_complex_including_files_to_included_files(self,
+      including_files_to_included_files):
+    including_files = including_files_to_included_files.keys()
+    including_files.sort()
+    self.assertEqual(including_files, ["bar/bar.h",
+                                       "bar/baz/bar_core_factory.h",
+                                       "foo/foo.h"])
+
+  def verify_complex_included_files_to_including_files(self,
+      included_files_to_including_files):
+    self.assertIn("bar/bar.h", included_files_to_including_files)
+    inclusions_of_bar = included_files_to_including_files["bar/bar.h"]
+    inclusions_of_bar.sort()
+    self.assertEqual(inclusions_of_bar, ["foo/foo.h"])
+
+    self.assertIn("bar/core.h", included_files_to_including_files)
+    inclusions_of_core = included_files_to_including_files["bar/core.h"]
+    inclusions_of_core.sort()
+    self.assertEqual(inclusions_of_core, ["bar/bar.h",
+                                          "bar/baz/bar_core_factory.h"])
+
   def test_map_including_files_to_included_files_basic(self):
     generator = inclusions_generator.InclusionsGenerator(BASIC_TEST_CONFIG)
     inclusions = generator.map_including_files_to_included_files()
@@ -47,27 +68,12 @@ class TestInclusionsGenerator(unittest.TestCase):
   def test_map_including_files_to_included_files_complex(self):
     generator = inclusions_generator.InclusionsGenerator(COMPLEX_TEST_CONFIG)
     inclusions = generator.map_including_files_to_included_files()
-
-    including_files = inclusions.keys()
-    including_files.sort()
-    self.assertEqual(including_files, ["bar/bar.h",
-                                       "bar/baz/bar_core_factory.h",
-                                       "foo/foo.h"])
+    self.verify_complex_including_files_to_included_files(inclusions)
 
   def test_map_included_files_to_including_files_complex(self):
     generator = inclusions_generator.InclusionsGenerator(COMPLEX_TEST_CONFIG)
     inclusions = generator.map_included_files_to_including_files()
-
-    self.assertIn("bar/bar.h", inclusions)
-    inclusions_of_bar = inclusions["bar/bar.h"]
-    inclusions_of_bar.sort()
-    self.assertEqual(inclusions_of_bar, ["foo/foo.h"])
-
-    self.assertIn("bar/core.h", inclusions)
-    inclusions_of_core = inclusions["bar/core.h"]
-    inclusions_of_core.sort()
-    self.assertEqual(inclusions_of_core, ["bar/bar.h",
-                                          "bar/baz/bar_core_factory.h"])
+    self.verify_complex_included_files_to_including_files(inclusions)
 
   def test_generate_inclusions_database_simple(self):
     generator = inclusions_generator.InclusionsGenerator(BASIC_TEST_CONFIG)
