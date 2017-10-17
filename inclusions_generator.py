0688c5a98b91ec91f4f0a515d064c1832e93dd79
Colin Blundell <blundell@chromium.org>
2017-10-17 11:02:04 +0200

    Flesh out impl and test

--- inclusions_generator.py
+++ inclusions_generator.py
@@ -1,7 +1,23 @@
+import subprocess
+
+# Given a repo root and a filename about which to generate inclusions,
+# returns the list of files that include |included_file| in the repo.
+def generateInclusionsForFile(repo_root, included_file):
+  inclusion_string = '\'include "\'' + included_file
+  inclusions = subprocess.Popen("git grep -l " + inclusion_string, shell=True, stdout=subprocess.PIPE, cwd=repo_root).stdout.read()
+  return inclusions.splitlines()
+
 # Given a configuration of files about which to generate inclusions, generates
 # the set of inclusions.
-# Params:
+# Params: A configuration as specified in XXX.
 # Returns: a dictionary mapping including filenames to lists of included
 # filenames.
-def GenerateInclusions():
+def generateInclusionsForConfig(config):
+  repo_root = config["repo_root"]
+  included_files = config["included_files"]
+
+  for included_file in included_files:
+    inclusions = generateInclusionsForFile(repo_root, included_file)
+    print inclusions
+
   return {}
--- test_inclusions_generator.py
+++ test_inclusions_generator.py
@@ -1,8 +1,19 @@
+import os
 import unittest
 
 import inclusions_generator
 
+TEST_CONFIG = {
+  "repo_root" : os.path.abspath("./test/data"),
+  "included_files" : [
+    "foo/foo.h",
+  ],
+}
+
 class TestInclusionsGenerator(unittest.TestCase):
   
-  def test_basic(self):
-    self.assertEqual(1, 1)
+  def test_generate_inclusions(self):
+    inclusions = inclusions_generator.generateInclusionsForConfig(TEST_CONFIG)
+    including_files = inclusions.keys()
+    including_files.sort()
+    self.assertEqual(including_files, ["bar/bar.h", "bar/core.h", "foo/foo.cc"])
