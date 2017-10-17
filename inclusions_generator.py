12c5575d619541ce41e8529892c2d2fcf9ce0e1c
Colin Blundell <blundell@chromium.org>
2017-10-17 11:05:06 +0200

    Get first test to pass

--- inclusions_generator.py
+++ inclusions_generator.py
@@ -4,8 +4,8 @@ import subprocess
 # returns the list of files that include |included_file| in the repo.
 def generateInclusionsForFile(repo_root, included_file):
   inclusion_string = '\'include "\'' + included_file
-  inclusions = subprocess.Popen("git grep -l " + inclusion_string, shell=True, stdout=subprocess.PIPE, cwd=repo_root).stdout.read()
-  return inclusions.splitlines()
+  including_files = subprocess.Popen("git grep -l " + inclusion_string, shell=True, stdout=subprocess.PIPE, cwd=repo_root).stdout.read()
+  return including_files.splitlines()
 
 # Given a configuration of files about which to generate inclusions, generates
 # the set of inclusions.
@@ -16,8 +16,11 @@ def generateInclusionsForConfig(config):
   repo_root = config["repo_root"]
   included_files = config["included_files"]
 
+  including_files_to_included_files = {}
   for included_file in included_files:
-    inclusions = generateInclusionsForFile(repo_root, included_file)
-    print inclusions
+    for including_file in generateInclusionsForFile(repo_root, included_file):
+      if including_file not in including_files_to_included_files:
+        including_files_to_included_files[including_file] = []
+      including_files_to_included_files[including_file].append(included_file)
 
-  return {}
+  return including_files_to_included_files
