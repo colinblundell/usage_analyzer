3f40fe04719e5df717db931da73fdb70ba3e8055
Colin Blundell <blundell@chromium.org>
2017-10-18 14:28:17 +0200

    Refactor generate_inclusions

--- generate_inclusions.py
+++ generate_inclusions.py
@@ -5,10 +5,13 @@ import sys
 import inclusions_config
 import inclusions_database
 
-if __name__ == '__main__':
-  config_filename = sys.argv[1]
-  output_dir = sys.argv[2]
+def generate_inclusions(config_filename, output_dir):
   config = inclusions_config.read_config_from_file(config_filename)
 
   inclusions_db = inclusions_database.generate_inclusions_database(config)
   inclusions_database.write_inclusions_db_to_disk(inclusions_db, output_dir)
+
+if __name__ == '__main__':
+  config_filename = sys.argv[1]
+  output_dir = sys.argv[2]
+  generate_inclusions(config_filename, output_dir)
