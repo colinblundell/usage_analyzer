17441b58182aea8adf6765b0c81ea0b32e98c1e2
Colin Blundell <blundell@chromium.org>
2017-10-19 16:21:09 +0200

    Add signin config

---
+++ data/configs/signin.py
@@ -0,0 +1,13 @@
+{
+  "name": "signin",
+  "repo_root" : "CHROMIUM_ROOT",
+  "included_files" : [
+    "components/signin/core/browser/account_fetcher_service",
+    "components/signin/core/browser/account_tracker_service",
+    "components/signin/core/browser/profile_oauth2_token_service",
+    "components/signin/core/browser/signin_manager_base",
+    "components/signin/core/browser/signin_manager",
+    "components/sync/driver/signin_manager_wrapper",
+    "google_apis/gaia/oauth2_token_service",
+  ]
+}
