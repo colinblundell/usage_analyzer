cd56c2f82ff96f96bda4ff58e88d9281121eb6f8
Colin Blundell <blundell@chromium.org>
2017-10-23 16:41:38 +0200

    more pylint

--- test_inclusions_generator.py
+++ test_inclusions_generator.py
@@ -2,27 +2,31 @@ import os
 import unittest
 
 import inclusions_generator
-from test_utils import *
+import test_utils
 
 
 class TestInclusionsGenerator(unittest.TestCase):
 
   def test_map_including_to_included_basic(self):
-    generator = inclusions_generator.InclusionsGenerator(BASIC_TEST_CONFIG)
+    generator = inclusions_generator.InclusionsGenerator(
+        test_utils.BASIC_TEST_CONFIG)
     inclusions = generator.map_including_to_included()
-    verify_basic_including_to_included(self, inclusions)
+    test_utils.verify_basic_including_to_included(self, inclusions)
 
   def test_map_included_to_including_basic(self):
-    generator = inclusions_generator.InclusionsGenerator(BASIC_TEST_CONFIG)
+    generator = inclusions_generator.InclusionsGenerator(
+        test_utils.BASIC_TEST_CONFIG)
     inclusions = generator.map_included_to_including()
-    verify_basic_included_to_including(self, inclusions)
+    test_utils.verify_basic_included_to_including(self, inclusions)
 
   def test_map_including_to_included_complex(self):
-    generator = inclusions_generator.InclusionsGenerator(COMPLEX_TEST_CONFIG)
+    generator = inclusions_generator.InclusionsGenerator(
+        test_utils.COMPLEX_TEST_CONFIG)
     inclusions = generator.map_including_to_included()
-    verify_complex_including_to_included(self, inclusions)
+    test_utils.verify_complex_including_to_included(self, inclusions)
 
   def test_map_included_to_including_complex(self):
-    generator = inclusions_generator.InclusionsGenerator(COMPLEX_TEST_CONFIG)
+    generator = inclusions_generator.InclusionsGenerator(
+        test_utils.COMPLEX_TEST_CONFIG)
     inclusions = generator.map_included_to_including()
-    verify_complex_included_to_including(self, inclusions)
+    test_utils.verify_complex_included_to_including(self, inclusions)
