09427539a61aee469fb354303fe0e8c832fb1566
Colin Blundell <blundell@chromium.org>
2017-10-24 17:06:34 +0200

    Add suffixes to signin config

--- data/configs/signin.py
+++ data/configs/signin.py
@@ -4,12 +4,12 @@
     "repo_root":
         "CHROMIUM_ROOT",
     "included_files": [
-        "components/signin/core/browser/account_fetcher_service",
-        "components/signin/core/browser/account_tracker_service",
-        "components/signin/core/browser/profile_oauth2_token_service",
-        "components/signin/core/browser/signin_manager_base",
-        "components/signin/core/browser/signin_manager",
-        "components/sync/driver/signin_manager_wrapper",
-        "google_apis/gaia/oauth2_token_service",
+        "components/signin/core/browser/account_fetcher_service.h",
+        "components/signin/core/browser/account_tracker_service.h",
+        "components/signin/core/browser/profile_oauth2_token_service.h",
+        "components/signin/core/browser/signin_manager_base.h",
+        "components/signin/core/browser/signin_manager.h",
+        "components/sync/driver/signin_manager_wrapper.h",
+        "google_apis/gaia/oauth2_token_service.h",
     ]
 }
