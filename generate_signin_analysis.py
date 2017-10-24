e61667d96ad8c9e951dcb6d176dcc1f9104cbc6c
Colin Blundell <blundell@chromium.org>
2017-10-24 11:01:39 +0200

    Add initial usage of per-feature analysis

--- generate_signin_analysis.py
+++ generate_signin_analysis.py
@@ -3,6 +3,7 @@
 import sys
 
 from including_to_included_analyzer import IncludingToIncludedAnalyzer
+import common_utils
 import signin_analysis_lib
 
 
@@ -13,7 +14,9 @@ def GenerateAnalysis(database_filename):
   output_csv = analyzer.GenerateGlobalAnalysisAsCsv(
       signin_analysis_lib.FilenameToSigninClient, "feature")
   print output_csv
-  print
+
+  signin_analysis = analyzer.GenerateAnalysis(lambda f: signin_analysis_lib.InClient(f, "signin"))
+  print common_utils.DictToCsv(signin_analysis, ["file", "num inclusions"], common_utils.DictKeysSortedByValue(signin_analysis))
 
 
 if __name__ == '__main__':
