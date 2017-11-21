4b95677baecefb4a4ffd1e24508f27dfeca1a0f0
Colin Blundell <blundell@chromium.org>
2017-11-21 16:23:12 +0100

    Add util to comput commit date of HEAD

--- git_utils.py
+++ git_utils.py
@@ -15,6 +15,16 @@ def GetRepoRevision(repo_root):
   return repo_rev_line.strip()
 
 
+# Returns the commit date of HEAD of |repo_root|.
+def GetRepoCommitDate(repo_root):
+  commit_date = subprocess.Popen(
+      "git show -s --date=short --format=%cd HEAD",
+      shell=True,
+      stdout=subprocess.PIPE,
+      cwd=repo_root).stdout.read()
+  return commit_date
+
+
 # Returns the shortrev of HEAD of the usage analyzer repo (i.e., this repo).
 def GetUsageAnalyzerRepoRevision():
   return GetRepoRevision(os.path.dirname(os.path.realpath(__file__)))
