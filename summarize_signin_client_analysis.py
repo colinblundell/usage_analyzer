b6d0c380847beb56e8d97e353cff4ed124068c31
Colin Blundell <blundell@chromium.org>
2017-11-29 15:00:54 +0100

    Update summary script

--- summarize_signin_client_analysis.py
+++ summarize_signin_client_analysis.py
@@ -46,5 +46,14 @@ for client_name, client_properties in signin_clients.iteritems():
   num_inclusions = client_num_inclusions[client_name]
   UpdateSummary(weighted_summary_of_clients, client_properties, num_inclusions)
 
-print summary_of_clients
-print weighted_summary_of_clients
+# Display the results in CSV format suitable for outputting into a spreadsheet.
+total_num_clients = len(signin_clients.keys())
+total_num_inclusions = client_num_inclusions["total"]
+print "Characteristic, %% of %d clients, %% of %d clients weighted by size" % (total_num_clients, total_num_inclusions)
+for key in summary_keys:
+  num_clients = summary_of_clients[key]
+  num_inclusions = weighted_summary_of_clients[key]
+  percent_of_clients = float(num_clients) / float(total_num_clients)
+  percent_of_inclusions = float(num_inclusions) / float(total_num_inclusions)
+  print "%s,%.3f,%.3f" % (key, percent_of_clients,
+                          percent_of_inclusions)
