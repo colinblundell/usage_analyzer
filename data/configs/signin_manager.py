dd58fb0cc3b9cac5c5a8ff715a2d8100f74039b1
Colin Blundell <blundell@chromium.org>
2018-11-23 17:10:46 +0100

    Update signin configs

---
+++ data/configs/signin_manager.py
@@ -0,0 +1,22 @@
+{
+    "name":
+        "signin_manager",
+    "repo_root":
+        "CHROMIUM_ROOT",
+    "included_files": [
+        "components/signin/core/browser/signin_manager.cc",
+        "components/signin/core/browser/signin_manager.h",
+        "components/signin/core/browser/signin_manager_base.cc",
+        "components/signin/core/browser/signin_manager_base.h",
+        "components/signin/core/browser/fake_signin_manager.cc",
+        "components/signin/core/browser/fake_signin_manager.h",
+        "chrome/browser/signin/signin_manager_factory.cc",
+        "chrome/browser/signin/signin_manager_factory.h",
+        "chrome/browser/signin/fake_signin_manager_builder.cc",
+        "chrome/browser/signin/fake_signin_manager_builder.h",
+        "ios/chrome/browser/signin/signin_manager_factory.cc",
+        "ios/chrome/browser/signin/signin_manager_factory.h",
+        "ios/chrome/browser/signin/fake_signin_manager_builder.cc",
+        "ios/chrome/browser/signin/fake_signin_manager_builder.h",
+    ]
+}
