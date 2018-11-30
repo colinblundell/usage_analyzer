04b6b09146af0ae313bec5dd82c87d5578df333d
Colin Blundell <blundell@chromium.org>
2018-11-30 11:00:03 +0100

    Added analyses for AccountTrackerService/GaiaCookieManagerService

---
+++ data/analysis_configs/signin/account_tracker_service.py
@@ -0,0 +1,12 @@
+{
+    "name":
+        "account_tracker_service",
+    "included_files_to_limit_to": [
+        "components/signin/core/browser/account_tracker_service.cc",
+        "components/signin/core/browser/account_tracker_service.h",
+        "chrome/browser/signin/account_tracker_service_factory.cc",
+        "chrome/browser/signin/account_tracker_service_factory.h",
+        "ios/chrome/browser/signin/account_tracker_service_factory.h",
+        "ios/chrome/browser/signin/account_tracker_service_factory.cc",
+    ]
+}
---
+++ data/analysis_configs/signin/gaia_cookie_manager_service.py
@@ -0,0 +1,18 @@
+{
+    "name":
+        "gaia_cookie_manager_service",
+    "included_files_to_limit_to": [
+        "components/signin/core/browser/gaia_cookie_manager_service.cc",
+        "components/signin/core/browser/gaia_cookie_manager_service.h",
+        "components/signin/core/browser/fake_gaia_cookie_manager_service.cc",
+        "components/signin/core/browser/fake_gaia_cookie_manager_service.h",
+        "chrome/browser/signin/gaia_cookie_manager_service_factory.cc",
+        "chrome/browser/signin/gaia_cookie_manager_service_factory.h",
+        "chrome/browser/signin/fake_gaia_cookie_manager_service_builder.cc",
+        "chrome/browser/signin/fake_gaia_cookie_manager_service_builder.h",
+        "ios/chrome/browser/signin/gaia_cookie_manager_service_factory.h",
+        "ios/chrome/browser/signin/gaia_cookie_manager_service_factory.cc",
+        "ios/chrome/browser/signin/fake_gaia_cookie_manager_service_builder.h",
+        "ios/chrome/browser/signin/fake_gaia_cookie_manager_service_builder.cc",
+    ]
+}
